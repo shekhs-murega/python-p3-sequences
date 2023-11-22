1ef59ca6cd9fd91d32f987d0babf6e07b96685bb
shekinah <nyamburamary1212@gmail.com>
2023-11-22 22:30:57 +0300

    Complete sequences task

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    else:
+        sequence = [0, 1]
+        if length == 1:
+            print([0])
+        elif length == 2:
+            print(sequence)
+        else:
+            while len(sequence) < length:
+                next_num = sequence[-1] + sequence[-2]
+                sequence.append(next_num)
+            print(sequence)
